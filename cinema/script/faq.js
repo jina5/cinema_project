1074f894325f9c9d004570885a3a852423c78fe1
dong hee <leedh7785@naver.com>
2022-06-18 14:40:43 +0900

    commit

--- cinema/script/faq.js
+++ cinema/script/faq.js
@@ -1,4 +1,24 @@
-        window.onload = () => {
+        
+        window.onload = function(){
+            var nav = document.querySelector('nav');
+            console.log(nav.offsetHeight);       
+            var sub_menu_container = document.querySelector('#sub_menu_container');         
+            var sub_menu_height = sub_menu_container.scrollHeight;
+            sub_menu_height += "px";
+            console.log(sub_menu_height);
+        
+            nav.onmouseenter = () => {
+                sub_menu_container.style.height = sub_menu_height;
+            }
+            nav.onmouseleave = () => {
+                sub_menu_container.style.height = "0px";
+            }
+            var sub_menu_li = document.querySelectorAll('#sub_menu_container a');
+            for(i=0;i<sub_menu_li.length;i++){
+                sub_menu_li[i].onclick = () => {
+                    sub_menu_container.style.height = "0px";
+                }
+            } // nav 자바스크립트
             var mainmenu = document.querySelectorAll('.mainmenu_line');
             var submenu = document.querySelectorAll('.subMenu');
             // console.log(submenu);
@@ -17,5 +37,5 @@
                         this.nextElementSibling.style.padding = "0px 0px";
                     }
                 }
-            }
-        }
+            } // faq 자바스크립트
+        } 
