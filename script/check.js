f673d237a4944a0d4501fc6c36dec09efda2687d
jina <jina.oh.dev@gmail.com>
2022-06-28 15:03:24 +0900

    commit

--- script/check.js
+++ script/check.js
@@ -1,19 +1,9 @@
+$(function(){
     $('.btn').click(function(){
         $('.popup_box').css('display', 'block');
-        $('.blur').css('display', 'block');
-    });
-    $('.btn_save').click(function(){
-        $('.popup_box').css('display', 'block');
-        $('.blur').css('display', 'block');
-    });
-    
+    })
     $('.close').click(function(){
         $('.popup_box').css('display', 'none');
         $('.blur').css('display', 'none');
     });
-    
-    $('.popup_cancle').click(function(){
-        $('.popup_box').css('display', 'none');
-        $('.blur').css('display', 'none');
-    });
-    
+})
